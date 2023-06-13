b9c75de29e045085c530b60a39229416ca19b835
Pragyan Das <36222744+PragyanD@users.noreply.github.com>
2023-06-13 01:44:30 -0500

    abu commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
+
+const dict = ['earth','prags','udees'];
+
+const state = {
+  secret: dict[Math.floor(Math.random()*dict.length)],
+  grid: Array(6)
+        .fill()
+        .map(() => Array(5).fill('')),
+  currentRow:0,
+  currentCol:0,
+}
+
+function updateGrid(){
+  for(let i = 0; i< state.grid.length;i++){
+    for(let j = 0; j < state.grid[i].length; j++){
+      const box = document.getElementById(`box${i}${j}`);
+      box.textContent = state.grid[i][j]
+    }
+  }
+
+}
 function drawBox(container, row, col, letter = '') {
     const box = document.createElement('div');
     box.className = 'box';
@@ -7,6 +28,7 @@ function drawBox(container, row, col, letter = '') {
     container.appendChild(box);
     return box;
   }
+
 function drawGrid(container) {
   const grid = document.createElement('div');
   grid.className = 'grid';
@@ -19,10 +41,79 @@ function drawGrid(container) {
 
   container.appendChild(grid);
 }
-
+function isLetter(key){
+    return key.length === 1 && key.match(/[a-z]/i)
+     
+}
+function addLetter(letter){
+  if (state.currentCol === 5) return;
+  state.grid[state.currentRow][state.currentCol] = letter;
+  state.currentCol++;
+}
+function removeLetter(){
+  if(state.currentCol === 0) return;
+  state.grid[state.currentRow][state.currentCol - 1] = '';
+  state,currentCol--;
+}
+function registerKeyboardEvents(){
+  document.body.onkeydown = (e) => {
+      const key = e.key;
+      if(key=='Enter'){
+          if(state.currentCol==5){
+            const word = getCurrentWord();
+            if (isWordValid(word)){
+              revealWord(word);
+              state.currentRow ++;
+              state.currentCol = 0;
+            } else{
+              alert('Not a valid word');
+            }
+          }
+      }
+      if(key=='Backspace'){
+        removeLetter();
+      }
+      if(isLetter(key)){
+        addLetter(key);
+      }
+      updateGrid();
+  };
+}
+function getCurrentWord(){
+  return state.grid[state.currentRow].reduce((prev,curr) => prev + curr);
+}
+function isWordValid(word){
+  return dict.includes(word);
+}
+function revealWord(guess){
+  const row = state.currentRow;
+  for(let i = 0; i < 5; i++){
+    const box = document.getElementById(`box${row}${i}`);
+    const letter = box.textContent;
+    if(letter == state.secret[i]){
+      box.classList.add('right');
+    } else if (state.secret.includes(letter)){
+      box.classList.add('wrong');
+    
+    } else{
+      box.classList.add('empty');
+    }
+  }
+  const isWinner = state.secret === guess;
+  const isGameOver = state.currentRow === 5;
+  if(isWinner){
+    alert('Congratulations!');
+  } else if (isGameOver){
+    alert(`Wrong guess, the word was ${state.secret}`);
+  }
+}
 function startup(){
     const game = document.getElementById('game');
     drawGrid(game);
-
+    
+    registerKeyboardEvents();
+    console.log(state.secret);
+    
 }
+
 startup();
